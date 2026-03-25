60c0f9f0efb4c1343c34b6c44aeca5811d601b81
adsxaluangit <adsxaluan@gmail.com>
2026-03-25 09:18:38 +0700

    Add diploma upload instructions for transfer students

--- fontend/App.tsx
+++ fontend/App.tsx
@@ -569,7 +569,7 @@ const App: React.FC = () => {
               <FileUpload label="CCCD mặt trước" required placeholderImage={files.frontId || "/assets/cccd_front.png"} onFileChange={(b64) => setFiles({ ...files, frontId: b64 })} />
               <FileUpload label="CCCD mặt sau" required placeholderImage={files.backId || "/assets/cccd_back.png"} onFileChange={(b64) => setFiles({ ...files, backId: b64 })} />
               <FileUpload label="Căn cước Điện tử" required placeholderImage={files.electronicId || "/assets/cccd_front.png"} helperText="Hướng dẫn: Vào VNeID > Thẻ căn cước > Căn cước điện tử > Chụp ảnh màn hình" onFileChange={(b64) => setFiles({ ...files, electronicId: b64 })} />
-              <FileUpload label="Bằng tốt nghiệp/GCN tốt nghiệp tạm thời" required placeholderImage={files.diploma || "/assets/diploma.png"} onFileChange={(b64) => setFiles({ ...files, diploma: b64 })} />
+              <FileUpload label="Bằng tốt nghiệp/GCN tốt nghiệp tạm thời" required placeholderImage={files.diploma || "/assets/diploma.png"} helperText="Liên thông trình độ Cao đẳng ngoài bằng tốt nghiệp THPT hoặc Giấy chứng nhận hoàn thành chương trình giáo dục phổ thông thì phải nộp bổ sung bằng tốt nghiệp Trung cấp" onFileChange={(b64) => setFiles({ ...files, diploma: b64 })} />
               <FileUpload label="Học bạ THPT/THCS" required placeholderImage={files.tempCert || "/assets/transcript.png"} onFileChange={(b64) => setFiles({ ...files, tempCert: b64 })} />
             </div>
           </FormSection>
